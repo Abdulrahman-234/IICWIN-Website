1db9f30e4486a58bf003af735b3178fc7675ed5a
Abdulrahman Bello <belloabdulrahman13@gmail.com>
2022-06-02 15:41:56 +0100

    Initial Commit

---
+++ readmore.js
@@ -0,0 +1,5 @@
+const read = document.querySelector("#readmore");
+const readText = document.querySelector("#readmore-text");
+read.addEventListener('click', () => {
+    readText.style.display = 'block'
+});
