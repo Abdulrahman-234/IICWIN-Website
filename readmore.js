5020bb4e685f78bbd3e0742be662b2fbeec0901b
Abdulrahman Bello <belloabdulrahman13@gmail.com>
2022-06-02 17:09:06 +0100

    Third Commit

--- readmore.js
+++ readmore.js
@@ -1,5 +1,22 @@
 const read = document.querySelector("#readmore");
-const readText = document.querySelector("#readmore-text");
-read.addEventListener('click', () => {
-    readText.style.display = 'block'
-});
+// const readText = document.querySelector("#readmore-text");
+// read.addEventListener('click', () => {
+//     readText.style.display = 'block'
+// });
+
+
+const readMore1 = document.querySelector('.readmore1');
+const readText1 = document.querySelector(".readmore-text1");
+readMore1.addEventListener('click',()=>{
+    readText1.style.display = 'block'
+})
+const readMore2 = document.querySelector('.readmore2');
+const readText2 = document.querySelector(".readmore-text2");
+readMore2.addEventListener('click',()=>{
+    readText2.style.display = 'block'
+})
+const readMore3 = document.querySelector('.readmore3');
+const readText3 = document.querySelector(".readmore-text3");
+readMore3.addEventListener('click',()=>{
+    readText3.style.display = 'block'
+})
